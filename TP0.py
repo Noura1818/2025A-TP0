8f930986348b2062feab219949c1b793f197957e
Noura1818 <noura.atmani@etud.polymtl.ca>
2025-09-06 20:45:45 -0400

    update TP0.py

--- TP0.py
+++ TP0.py
@@ -1,1 +1,8 @@
-# Écrire votre code ici
+import math as m
+
+# transformation du input en int (entier)
+r = int(input('Entrer le rayon (en mm):'))
+#formule de l'aire d'un cercle (importer math en m pour mettre la valeur pi)
+Aire = (m.pi)*(r**2)
+#print l'air dun cercle en transformant la formule Aire en string(str) pour le rendre compatible avec le string (texte)
+print('Laire du cercle est (en mm2) : ' + str(Aire))
